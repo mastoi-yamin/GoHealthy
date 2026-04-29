2f80ccb53fc66f753b3c9d702f778ef1252dde7e
Yamin Alee <yaminalee.ic@gmail.com>
2026-04-29 19:47:48 +0500

    fixed gitignore

--- server.js
+++ server.js
@@ -45,4 +45,5 @@ app.put("/edit/:id", (req, res) => {
   res.json({ msg: "Edited" });
 });
 
-app.listen(3000, () => console.log("http://localhost:3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running"));
