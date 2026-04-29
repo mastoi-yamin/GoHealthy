152c32b051c25b8ad393a5fa16e8dea8d63f79e7
Yamin Alee <yaminalee.ic@gmail.com>
2026-04-29 22:18:43 +0500

    fix root route

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/dashboard.html");
+});
+
 let requests = [];
 
 // GET
